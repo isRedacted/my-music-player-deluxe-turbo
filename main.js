83f1bbf48ab0da4af7a26bd7432561a148500607
isRedacted <farmoffarmers@gmail.com>
2025-04-29 19:01:12 +1000

    Added electron window manager

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
-const { app, BrowserWindow } = require('electron/main')
+const electron = require('electron');
+const app = electron.app;
+const fs = require('fs');
+const windowManager = require('electron-window-manager')
+const settingsFile = '/settings.json'
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+const createInitialWindow = () => {
+	var library = '';
+	windowManager.setDefaultSetup(
+		{
+			'width': 640,
+			'height': 360
+		}
+	);
 
-  win.loadFile('index.html')
+	// Get settings file, or open library dialog if non existent
+	if (fs.existsSync(settingsFile)) {
+		fs.readFile(settingsFile, 'utf-8', (err, data) => {
+			if (err) {
+				console.error('Something has gone catastrophically wrong:', err);
+				return;
+			} else {
+				var settings = JSON.parse(data);
+				library = settings.library
+			}
+		})
+		windowManager.open(undefined, undefined, '/index.html', false, {
+			'width': 1280,
+			'height': 720,
+			'resizable': true
+		});
+	} else {
+		windowManager.open(undefined, undefined, '/no_library.html');
+	}
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+app.on('ready', () => {
+	windowManager.init();
+	createInitialWindow();
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 })
