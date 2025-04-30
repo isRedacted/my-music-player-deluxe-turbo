71a376ab702ee5dc3ae6939aa6ae2d94f3dfa7cc
isRedacted <farmoffarmers@gmail.com>
2025-04-30 22:52:50 +1000

    Renamed

--- no_library_renderer.js
+++ no_library_renderer.js
@@ -4,3 +4,10 @@ document.getElementById("libraryPicker").addEventListener('click', async () => {
     const folder = await window.electronAPI.selectFolder();
     document.getElementById("libraryInput").innerHTML = folder;
 });
+
+document.getElementById("libraryProceed").addEventListener('click', () => {
+    libraryDir = document.getElementById("libraryInput").value;
+    if (libraryDir != null) {
+        
+    }
+});
