0b05a23b52309ddaf4623f148c7502c307ccc430
isRedacted <farmoffarmers@gmail.com>
2025-05-01 12:48:14 +1000

    Finalised library picker menu

--- main.js
+++ main.js
@@ -1,40 +1,58 @@
 const { app, ipcMain, dialog } = require('electron');
 const path = require('path');
 const windowManager = require('electron-window-manager');
-const settingsFile = path.join(__dirname, '/settings.json');
 const fs = require('fs');
+const { error } = require('console');
+const { workerData } = require('worker_threads');
+
+const settingsFile = path.join(__dirname, '/settings.json');
+// Read settings, throw an error if not found
+function readSettings() {
+	const settings = (fs.readFileSync(settingsFile, { encoding: 'utf-8' }));
+	return JSON.parse(settings);
+}
+// Read settings and/or write to file
+function writeSettings(key, value) {
+	let settings = {};
+	try {
+		settings = readSettings();
+	} finally {
+		settings[key] = value;
+		fs.writeFileSync(settingsFile, JSON.stringify(settings));
+	}
+}
+
+// Set window templates
+windowManager.setDefaultSetup(
+	{
+		'width': 640,
+		'height': 360,
+		'webPreferences': {
+			'preload': path.join(__dirname, 'preload.js')
+		}
+	}
+);
+windowManager.templates.set('main', {
+	'width': 1280,
+	'height': 720,
+	'resizable': true,
+	'webPreferences': {
+		'preload': path.join(__dirname, 'preload.js')
+	}
+});
 
 const createInitialWindow = () => {
-	var library = '';
-	windowManager.setDefaultSetup(
-		{
-			'width': 640,
-			'height': 360,
-			'showDevTools': true,
-			'webPreferences': {
-				'preload': path.join(__dirname, 'preload.js')
-			}
-		}
-	);
 
 	// Get settings file and open main window, or open library dialog window if non existent
-	if (fs.existsSync(settingsFile)) {
-		fs.readFile(settingsFile, 'utf-8', (err, data) => {
-			if (err) {
-				console.error('Something has gone catastrophically wrong:', err);
-				return;
-			} else {
-				var settings = JSON.parse(data);
-				library = settings.library
-			}
-		})
-		windowManager.open(undefined, undefined, '/index.html', {
-			'width': 1280,
-			'height': 720,
-			'resizable': true
-		});
-	} else {
-		windowManager.open(undefined, undefined, '/no_library.html');
+	try {
+		let settings = readSettings();
+		if (!settings.hasOwnProperty('libraryDir')) {
+			throw new Error
+		} else {
+			windowManager.open('main', false, '/index.html', 'main');
+		}
+	} catch (err) {
+		windowManager.open('noLibrary', false, '/no_library.html');
 	}
 }
 
@@ -53,7 +71,7 @@ ipcMain.handle('dialog:openFolder', async () => {
 ipcMain.handle('write-settings-sync', (event, key, value) => {
 	let settings;
 	try {
-		settings = JSON.parse(fs.readFileSync(settingsFile, { encoding: 'utf-8' }));
+		settings = readSettings();
 	} catch (err) {
 		settings = {};
 	}
@@ -62,8 +80,14 @@ ipcMain.handle('write-settings-sync', (event, key, value) => {
 })
 
 // Open new window
-ipcMain.on('open-main-window', () => {
+ipcMain.on('open-main-window', (event) => {
 	//TODO: Close the library window and open the main window
+	windowManager.closeAll();
+	windowManager.createNew(undefined, undefined, '/index.html', false, {
+		'width': 1280,
+		'height': 720,
+		'resizable': true
+	});
 })
 
 app.on('ready', () => {
--- no_library_renderer.js
+++ no_library_renderer.js
@@ -1,15 +1,14 @@
-const remote = require('remote');
-const windowManager = require('electron-window-manager');
-
 // Library icon pick
-document.getElementById("libraryPicker").addEventListener('click', async () => {
+document.getElementById("libraryBox").addEventListener('click', async () => {
     const folder = await window.electronAPI.selectFolder();
-    document.getElementById("libraryInput").innerHTML = folder;
+    if (folder != null) {
+        document.getElementById("libraryInput").innerHTML = folder;
+    }
 });
 
 document.getElementById("libraryProceed").addEventListener('click', async () => {
     libraryDir = document.getElementById("libraryInput").textContent;
-    if (libraryDir != null) {
+    if (libraryDir != '') {
         await window.electronAPI.writeSettingsSync("libraryDir", libraryDir);
         window.electronAPI.openMainWindow();
     }
