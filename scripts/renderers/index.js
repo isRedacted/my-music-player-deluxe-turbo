ba7e5fae5ab93e39face1e5192b28f4c09e03f32
isRedacted <farmoffarmers@gmail.com>
2025-05-02 21:26:31 +1000

    Started work on populating function

--- scripts/renderers/index.js
+++ scripts/renderers/index.js
@@ -1,5 +1,12 @@
+import { join } from "path";
+
+const rootDir = await window.electronAPI.rootDir();
+
 // TODO: Populate with pages
-async function populateBody () {
+async function populateBody() {
+    const settings = await window.electronAPI.readSettings();
+    // TODO: Check settings for last window else default to home
+    const sidebar = await fetch(join(rootDir, 'pages', 'sidebar.html'));
 }
 
 document.addEventListener('DOMContentLoaded', populateBody);
