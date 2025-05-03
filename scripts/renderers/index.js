e8277830a09fb819123ae55869644439f18cb0a3
isRedacted <farmoffarmers@gmail.com>
2025-05-03 22:38:07 +1000

    Fix for remote javascript not being used

--- scripts/renderers/index.js
+++ scripts/renderers/index.js
@@ -1,17 +1,33 @@
-const sidebar = document.getElementById('sidebar');
-const mainContentArea = document.getElementById('main-content-area');
-
 async function populateBody() {
     const rootDir = await window.electronAPI.rootDir();
     const sidebarURL = await window.electronAPI.join(rootDir, 'pages', 'sidebar.html');
-    const sidebarFetch = await fetch(sidebarURL);
-    sidebar.innerHTML = await sidebarFetch.text();
+    fetchAndInjectHTML(sidebarURL, 'sidebar');
     // TODO: Check settings for last window else default to home (WILL DO WHEN PLAYLIST VIEWS ARE IMPLEMENTED)
 }
 
+function fetchAndInjectHTML(url, containerId) {
+    fetch(url)
+        .then(res => res.text())
+        .then(html => {
+            const container = document.getElementById(containerId);
+            container.innerHTML = html;
+
+            // Extract and re-execute scripts
+            const scripts = container.querySelectorAll('script');
+            scripts.forEach(oldScript => {
+                const newScript = document.createElement('script');
+                [...oldScript.attributes].forEach(attr => {
+                    newScript.setAttribute(attr.name, attr.value);
+                });
+                newScript.textContent = oldScript.textContent;
+                oldScript.replaceWith(newScript);
+            });
+        });
+}
+
 // TODO: Functions to change window to home and to different playlists
-function toHome(){
-    
+function toHome() {
+
 };
 
 function toPlaylist(playlistURL) {
