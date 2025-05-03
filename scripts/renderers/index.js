a8eff342db208e2bbbf4a3e8b65b8af9e2d8f4f9
isRedacted <farmoffarmers@gmail.com>
2025-05-03 13:05:17 +1000

    Started work on populating main screen

--- scripts/renderers/index.js
+++ scripts/renderers/index.js
@@ -1,12 +1,12 @@
-import { join } from "path";
-
-const rootDir = await window.electronAPI.rootDir();
+const sidebar = document.getElementById('sidebar');
+const mainContentArea = document.getElementById('main-content-area');
 
 // TODO: Populate with pages
 async function populateBody() {
-    const settings = await window.electronAPI.readSettings();
-    // TODO: Check settings for last window else default to home
-    const sidebar = await fetch(join(rootDir, 'pages', 'sidebar.html'));
+    const rootDir = await window.electronAPI.rootDir();
+    const sidebarURL = await window.electronAPI.join(rootDir, 'pages', 'sidebar.html');
+    // TODO: Use fetch to load sidebar
+    // TODO: Check settings for last window else default to home (WILL DO WHEN PLAYLIST VIEWS ARE IMPLEMENTED)
 }
 
 document.addEventListener('DOMContentLoaded', populateBody);
