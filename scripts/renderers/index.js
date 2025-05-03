0a31d13d7bd5200d259436956715ae033b881faa
isRedacted <farmoffarmers@gmail.com>
2025-05-03 19:51:01 +1000

    FInished sidebar fetch and added placeholder window change functions

--- scripts/renderers/index.js
+++ scripts/renderers/index.js
@@ -5,8 +5,19 @@ const mainContentArea = document.getElementById('main-content-area');
 async function populateBody() {
     const rootDir = await window.electronAPI.rootDir();
     const sidebarURL = await window.electronAPI.join(rootDir, 'pages', 'sidebar.html');
+    const sidebarFetch = await fetch(sidebarURL);
+    sidebar.innerHTML = await sidebarFetch.text();
     // TODO: Use fetch to load sidebar
     // TODO: Check settings for last window else default to home (WILL DO WHEN PLAYLIST VIEWS ARE IMPLEMENTED)
 }
+
+// TODO: Functions to change window to home and to different playlists
+function toHome(){
+    
+};
+
+function toPlaylist(playlistURL) {
+
+};
 
 document.addEventListener('DOMContentLoaded', populateBody);
